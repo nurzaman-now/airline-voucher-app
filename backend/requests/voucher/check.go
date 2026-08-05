01f4f2d1c2f37b606bfb60d5bb82186545a89e7d
nurzaman-now <imannj69@gmail.com>
2026-08-05 08:10:37 +0700

    feat(backend): Validasi format tanggal pada request voucher

--- backend/requests/voucher/check.go
+++ backend/requests/voucher/check.go
@@ -3,5 +3,5 @@ package voucher
 // CheckVoucherRequest untuk endpoint POST /api/check
 type CheckVoucherRequest struct {
 	FlightNumber string `json:"flight_number" binding:"required"`
-	FlightDate   string `json:"flight_date" binding:"required"`
+	FlightDate   string `json:"flight_date" binding:"required,datetime=2006-01-02 15:04:05"`
 }
--- backend/requests/voucher/create.go
+++ backend/requests/voucher/create.go
@@ -5,6 +5,6 @@ type CreateVoucersRequest struct {
 	CrewName     string `json:"crew_name" binding:"required"`
 	CrewId       string `json:"crew_id" binding:"required"`
 	FlightNumber string `json:"flight_number" binding:"required"`
-	FlightDate   string `json:"flight_date" binding:"required"`
+	FlightDate   string `json:"flight_date" binding:"required,datetime=2006-01-02 15:04:05"`
 	AircraftType string `json:"aircraft_type" binding:"required"`
 }
